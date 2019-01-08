770f994cd58bad8eebef342c7e10c7570ef71726
Chance Hudson <jchancehud@gmail.com>
2019-01-08 12:25:50 -0600

    Add info and settle button

--- src/components/PaymentCell.tsx
+++ src/components/PaymentCell.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { inject, observer } from 'mobx-react';
 import { BlockContainer, BlockElement, BlockHeader, BlockFooter } from './Shared';
 import styled from 'styled-components';
-import { Payment } from '../stores/Syndicate';
+import SyndicateStore, { Payment } from '../stores/Syndicate';
+import EthereumStore from '../stores/Ethereum';
 
 const TextSpan = styled.span`
   margin-left: 4px;
   margin-right: 4px;
 `;
 
+@inject('syndicateStore', 'ethereumStore')
+@observer
 export default class PaymentCell extends React.Component <{
-  payment: Payment
+  payment: Payment,
+  ethereumStore?: EthereumStore,
+  syndicateStore?: SyndicateStore
 }> {
   render() {
     return (
@@ -18,7 +24,20 @@ export default class PaymentCell extends React.Component <{
           <TextSpan>Payment - {this.props.payment.receiver}</TextSpan>
         </BlockHeader>
         <BlockElement>
-          Test
+          Sender: {this.props.payment.sender}
+          <br />
+          Receiver: {this.props.payment.receiver}
+          <br />
+          WeiValue: {this.props.payment.weiValue}
+          <br />
+          WeiPaid: {this.props.payment.weiPaid}
+          <br />
+          <button type="button" onClick={() => {
+            this.props.syndicateStore.settlePayment(
+              this.props.ethereumStore.activeAddress,
+              1 // TODO replace with number
+            );
+          }}>Settle</button>
         </BlockElement>
         <BlockFooter>
         </BlockFooter>
