55704c3a45901ec6905d0ac2da90cf71ad903fe8
Chance Hudson <jchancehud@gmail.com>
2019-01-08 10:54:05 -0600

    Break web3 initialization into own file

---
+++ src/web3.ts
@@ -0,0 +1,11 @@
+import Web3 from 'web3';
+/**
+ * Initialize a global web3 from metamask or from a fallback source.
+ **/
+if (typeof web3 !== 'undefined') {
+  global.web3 = new Web3(web3.currentProvider);
+} else {
+  // Set the provider you want from Web3.providers
+  console.log('Consider installing metamask!');
+  global.web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.commontheory.io'));
+}
