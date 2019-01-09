e78a42ecfdd7b6920a336b00b810139d51408392
Chance Hudson <jchancehud@gmail.com>
2019-01-08 23:47:06 -0600

    Round ether display to 5 decimal places

--- src/components/WeiDisplay.tsx
+++ src/components/WeiDisplay.tsx
@@ -6,9 +6,11 @@ export default class WeiDisplay extends React.Component <{
   wei: BN | number | string
 }> {
   render() {
+    const etherValue = +web3.utils.fromWei(this.props.wei.toString());
+    const roundedEtherValue = Math.round(etherValue * 1e5) / 1e5;
     return (
       <>
-        {`${web3.utils.fromWei(this.props.wei.toString()).toString()} ether`}
+        {`${roundedEtherValue.toString()} ether`}
       </>
     );
   }
