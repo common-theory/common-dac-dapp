7c79395520e1a52b5ecc77d5dc18d8980e2950c9
Chance Hudson <jchancehud@gmail.com>
2019-01-11 11:27:45 -0600

    Update rinkeby address

--- src/stores/Syndicate.ts
+++ src/stores/Syndicate.ts
@@ -173,7 +173,7 @@ export default class SyndicateStore {
     if (networkId === 1) {
       return '0x3c0Dc66381A4e40d9afBfc33F74e503eB8099F27';
     } else if (networkId === 4) {
-      return '0xbc529e7b0036184e25b2ad69e7b19672dfb5dc0c';
+      return '0xecc094eb4f7b466f9620b9a6b663763b9c53f99d';
     } else {
       throw new Error(`Invalid networkId: ${networkId} supplied to addressForNetwork`);
     }
