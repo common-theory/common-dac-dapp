6105fae94ecc37b4c89ffc295b0a48892a521c99
Chance Hudson <jchancehud@gmail.com>
2019-01-22 16:48:55 -0600

    Remove stray semicolon

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -37,7 +37,7 @@ export default class Home extends React.Component <{
               return (
                 <PaymentCell payment={payment} key={i} />
               );
-            })};
+            })}
           </div>
           <Footer />
         </Container>
