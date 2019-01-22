338c26745a32c79c66303543c4ccd81fa20144aa
Chance Hudson <jchancehud@gmail.com>
2019-01-22 17:32:36 -0600

    Add address checking to AddressField

--- src/components/AddressField.tsx
+++ src/components/AddressField.tsx
@@ -10,17 +10,39 @@ const AddressInput = styled.input`
 
 export default class AddressField extends React.Component<{
   address: string,
+  isAddressValid: boolean
   onChange: (newAddress: string) => void
 }> {
+  state = {
+    isAddressValid: false
+  };
+
+  addressChanged = (text: string) => {
+    this.setState({
+      isAddressValid: web3.utils.isAddress(text)
+    });
+    this.props.onChange(text);
+  };
+
+  renderAddressValidity() {
+    if (!this.props.address) return null;
+    if (this.state.isAddressValid) {
+      return ' Valid Address';
+    } else {
+      return ' Invalid Address';
+    }
+  }
+  
   render() {
     return (
       <>
         <AddressInput
           placeholder="0x7726A9b0E93dE68bf24d40b37F6D0DC4e4caF47C"
           name="address"
-          onChange={event => this.props.onChange(event.target.value)}
+          onChange={event => this.addressChanged(event.target.value)}
           value={this.props.address}
         />
+        {this.renderAddressValidity()}
       </>
     );
   }
