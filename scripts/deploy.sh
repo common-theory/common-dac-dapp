b795754bc6205ee2b25273f76f4f0b7112040bad
Chance Hudson <jchancehud@gmail.com>
2019-01-24 03:52:56 -0600

    Update deploy script to be used in non-ci settings

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,29 +2,30 @@
 
 set -e
 
-# Intended to be run in a CI environment
-if [ -z "$CI" ];
-then
-  echo 'Non-ci environment detected, exiting'
-  exit 1
-fi
+# Load from .env
+set -o allexport
+[ -f .env ] && source .env
+set +o allexport
 
 # Build the web app in ./static
 npm run build:production
 
-# Install jsipfs
-npm i -g ipfs
+# Ensure jsipfs executable is present
+which jsipfs > /dev/null
 
 # Start a local IPFS node
-jsipfs init
 jsipfs daemon &
+JSPID=$!
+
 sleep 10
+# Check that the process is up
+ps -ax | grep $JSPID | grep -v grep > /dev/null
 
 DOMAIN=commontheory.io
 CIDHOOKD_URL=cidhookd.commontheory.io
 
 # Load the old CID based on the current dnslinked value
-OLD_CID=$(jsipfs dns $DOMAIN)
+OLD_CID=$(npx dnslink resolve $DOMAIN)
 
 # Load the new CID by adding it to the local IPFS node
 NEW_CID=$(jsipfs add -Qr ./static)
@@ -38,4 +39,6 @@ npx cidhook $CIDHOOKD_URL $NEW_CID
 # Update the DNS record
 npx dnslink update $DOMAIN $NEW_CID
 
-wget $DOMAIN
+curl $DOMAIN > /dev/null 2> /dev/null
+
+kill $JSPID
