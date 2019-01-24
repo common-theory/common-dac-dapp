13c1e3e1aaf944b858252f328b30c9750467166b
Chance Hudson <jchancehud@gmail.com>
2019-01-24 07:50:25 -0600

    Style TextInput

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,8 +1,17 @@
+import React from 'react';
 import styled from 'styled-components';
 
-export default styled.input`
+const Input = styled.input`
   margin: 4px;
   font-family: Helvetica;
   border-radius: 3px;
   border: 1px solid black;
+  padding: 2px;
 `;
+
+export default (props: any) => (
+  // Set an random string to get chrome to not autocomplete
+  <Input { ...props } type="none" autocomplete="rutjfkde">
+    {props.children}
+  </Input>
+);
