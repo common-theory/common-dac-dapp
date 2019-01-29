5c8eb97264ecec540cd6ba3f29a5251fcaf9939e
Chance Hudson <jchancehud@gmail.com>
2019-01-28 19:30:58 -0600

    Withdraw -> Settle

--- src/components/PaymentInfo.tsx
+++ src/components/PaymentInfo.tsx
@@ -66,7 +66,7 @@ export default class PaymentInfo extends React.Component<{
                   this.props.payment.index
                 );
               }}>
-              Withdraw
+              Settle Payment
               </Button>
             )
           }
